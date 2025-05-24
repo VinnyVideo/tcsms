99cd245f31e105ac5c9bd5070cee03a9f13a1e9e
VinnyVideo <168298103+VinnyVideo@users.noreply.github.com>
2025-05-23 21:14:48 -0400

    W3C Validator fixes
    Tweaked a few things with inputs that the W3C Validator complains about

--- template/base/temp_login.php
+++ template/base/temp_login.php
@@ -8,7 +8,7 @@ return <<<HTML
 <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 <div class="sform">
 <form method="post" action="{$reg_url}">
-<input type="hidden" name="security_token" value="{$token}" />
+<input type="hidden" name="security_token" value="{$token}">
 <div class="sformstrip">Welcome to registration</div>
 <div class="sformblock">Registering an account will allow you to submit files or comment on other user's work, among
   other things.  Fields marked with an asterisk (<span class="highlight">*</span>) are required.
@@ -19,27 +19,27 @@ return <<<HTML
 <table class="sformtable">
 <tr>
   <td class="sformleft"><label for="username">Username</label><span class="highlight">*</span></td>
-  <td class="sformright"><input type="text" id="username" name="username" size="40" class="textbox" /></td>
+  <td class="sformright"><input type="text" id="username" name="username" size="40" class="textbox"></td>
 </tr>
 <tr>
   <td class="sformleft"><label for="password">Password</label> (8 characters, at least 1 letter and 1 number)<span class="highlight">*</span></td>
-  <td class="sformright"><input type="password" id="password" name="password" size="40" class="textbox" /></td>
+  <td class="sformright"><input type="password" id="password" name="password" size="40" class="textbox"></td>
 </tr>
 <tr>
   <td class="sformleft"><label for="email">Email</label><span class="highlight">*</span></td>
-  <td class="sformright"><input type="text" id="email" name="email" size="40" class="textbox" /></td>
+  <td class="sformright"><input type="text" id="email" name="email" size="40" class="textbox"></td>
 </tr>
 <tr>
   <td class="sformleft"><label for="image">Avatar URL</label></td>
-  <td class="sformright"><input type="text" id="image" name="image" size="40" value="" class="textbox" /></td>
+  <td class="sformright"><input type="text" id="image" name="image" size="40" value="" class="textbox"></td>
 </tr>
 <!--<tr>
   <td class="sformleft">Website Name</td>
-  <td class="sformright"><input type="text" name="website" size="40" class="textbox" /></td>
+  <td class="sformright"><input type="text" name="website" size="40" class="textbox"></td>
 </tr>
 <tr>
   <td class="sformleft">Website Address</td>
-  <td class="sformright"><input type="text" name="weburl" value="http://" size="40" class="textbox" /></td>
+  <td class="sformright"><input type="text" name="weburl" value="http://" size="40" class="textbox"></td>
 </tr>-->
 </table>
 <div class="sformstrip">Security Check</div>
@@ -49,14 +49,14 @@ return <<<HTML
   <td class="sformright">
     <!--<table border="0" cellpadding="0" cellspacing="0">
       <tr><td align="center"><img src="{$STD->tags['root_url']}act=main&param=12&dd={$token}" /></td></tr>
-      <tr><td align="center"><input type="text" name="regcode" size="6" class="textbox" style="margin-top: 3px" /></td></tr>
+      <tr><td align="center"><input type="text" name="regcode" size="6" class="textbox" style="margin-top: 3px"></td></tr>
     </table>-->
     <div class="g-recaptcha" data-sitekey="6LeCUbIUAAAAAJyMRDz9a-xWkczsmXNNg_Awxurk"></div>
   </td>
 </tr>
 </table>
 <div class="sformstrip" style="text-align:center">
-  <input type="submit" name="submit" value="Register" class="button" />
+  <input type="submit" name="submit" value="Register" class="button">
 </div>
 </form>
 </div>
@@ -68,7 +68,7 @@ global $STD;
 return <<<HTML
 <div class="sform">
 <form method="post" action="{$reg_url}">
-<input type="hidden" name="security_token" value="{$token}" />
+<input type="hidden" name="security_token" value="{$token}">
 <div class="sformstrip">Recover from a lost password</div>
 <div class="sformblock">Use this form to change your password if you've forgotton it.  A link to a page where you can
   choose a new password will be emailed to the address on your account.
@@ -77,10 +77,10 @@ return <<<HTML
 <table class="sformtable">
 <tr>
   <td class="sformleft"><label for="username">Your username</label></td>
-  <td class="sformright"><input type="text" id="username" name="username" size="40" class="textbox" /></td>
+  <td class="sformright"><input type="text" id="username" name="username" size="40" class="textbox"></td>
 </tr>
 </table>
-<div class="sformstrip" style="text-align:center"><input type="submit" value="Submit" class="button" /></div>
+<div class="sformstrip" style="text-align:center"><input type="submit" value="Submit" class="button"></div>
 </div>
 </form>
 </div>
@@ -92,24 +92,24 @@ global $STD;
 return <<<HTML
 <div class="sform">
 <form method="post" action="{$reg_url}">
-<input type="hidden" name="security_token" value="{$token}" />
-<input type="hidden" name="cookie" value="{$cookie}" />
+<input type="hidden" name="security_token" value="{$token}">
+<input type="hidden" name="cookie" value="{$cookie}">
 <div class="sformstrip">Change your password</div>
 <table class="sformtable">
 <tr>
   <td class="sformleft">Enter your username</td>
-  <td class="sformright"><input type="text" name="username" size="40" class="textbox" /></td>
+  <td class="sformright"><input type="text" name="username" size="40" class="textbox"></td>
 </tr>
 <tr>
   <td class="sformleft">Enter new password</td>
-  <td class="sformright"><input type="password" name="pass1" size="40" class="textbox" /></td>
+  <td class="sformright"><input type="password" name="pass1" size="40" class="textbox"></td>
 </tr>
 <tr>
   <td class="sformleft">Retype new password</td>
-  <td class="sformright"><input type="password" name="pass2" size="40" class="textbox" /></td>
+  <td class="sformright"><input type="password" name="pass2" size="40" class="textbox"></td>
 </tr>
 </table>
-<div class="sformstrip" style="text-align:center"><input type="submit" class="button" value="Submit" /></div>
+<div class="sformstrip" style="text-align:center"><input type="submit" class="button" value="Submit"></div>
 </div>
 HTML;
 }
