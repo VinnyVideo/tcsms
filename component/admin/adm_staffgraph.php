8a36bf2b17b40ff55ac7e3861468f6a06a370cc1
VinnyVideo <168298103+VinnyVideo@users.noreply.github.com>
2025-05-27 19:14:27 -0400

    W3C Validator tweaks
    Make W3C Validator happy with adm_staffgraph

--- component/admin/adm_staffgraph.php
+++ component/admin/adm_staffgraph.php
@@ -14,9 +14,9 @@ class component_adm_staffgraph {
 		
 		$this->output .= $STD->global_template->page_header("Staff Graph");
 		if (empty($_GET['time']))
-			$this->output .= $STD->global_template->message("<img src=\"/admin.php?act=staffgraphimage\" alt='Staff Graph'/>");
+			$this->output .= $STD->global_template->message("<img src=\"/admin.php?act=staffgraphimage\" alt='Staff Graph'>");
 		else
-			$this->output .= $STD->global_template->message("<img src=\"/admin.php?act=staffgraphimage&time=".$_GET['time']."\" alt='Staff Graph' />");
+			$this->output .= $STD->global_template->message("<img src=\"/admin.php?act=staffgraphimage&time=".$_GET['time']."\" alt='Staff Graph'>");
 
 		$STD->template->display( $this->output );
 	}
