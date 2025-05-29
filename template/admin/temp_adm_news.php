5cb10c975ed54a1ef930e6a778c6412ef7cf742e
VinnyVideo <168298103+VinnyVideo@users.noreply.github.com>
2025-05-28 22:33:14 -0400

    Make news fields wider, fix deprecated HTML
    This gives you a lot more space to see news entries, so you won't have to scroll as much to see the entire news post. Also, I fixed deprecated HTML and added a missing label for one version of the editor.

--- template/admin/temp_adm_news.php
+++ template/admin/temp_adm_news.php
@@ -15,25 +15,25 @@ global $STD;
 return <<<HTML
 Write a new news entry here.  {%recent_updates%} will insert the Recent Updates into your message.
 <form method="post" action="{$STD->tags['root_url']}act=news&amp;param=02">
-<input type="hidden" name="security_token" value="{$token}" />
-<div align="center">
+<input type="hidden" name="security_token" value="{$token}">
+<div style="margin-left:20px;">
 <br>
-<table cellspacing="0" cellpadding="1" style="width:90%;">
+<table style="border-spacing:0px;width:90%;">
 <tr>
-  <td width="30%" class="title"><label for="title">Title</label></td>
-  <td width="70%" class="field"><input type="text" id="title" name="title" size="60" class="textbox" /></td>
+  <td style="width:20%;" class="title"><label for="title">Title</label></td>
+  <td style="width:80%;" class="field"><input type="text" id="title" name="title" size="60" class="textbox"></td>
 </tr>
 <tr>
-  <td width="30%" class="title"><label for="content">Content</label></td>
-  <td width="70%" class="field"><textarea id="content" name="content" rows="8" cols="80" class="textbox"></textarea></td>
+  <td style="width:20%;" class="title"><label for="content">Content</label></td>
+  <td style="width:80%;" class="field"><textarea id="content" name="content" rows="14" cols="80" class="textbox"></textarea></td>
 </tr>
 <tr>
-  <td width="30%" class="title">&nbsp;</td>
-  <td width="70%" class="field">&nbsp;</td>
+  <td style="width:20%;" class="title">&nbsp;</td>
+  <td style="width:80%;" class="field">&nbsp;</td>
 </tr>
 <tr>
-  <td width="30%" class="title">Complete Form</td>
-  <td width="70%" class="field"><input type="submit" value="Add Entry" class="button" /></td>
+  <td style="width:20%;" class="title">Complete Form</td>
+  <td style="width:80%;" class="field"><input type="submit" value="Add Entry" class="button"></td>
 </tr>
 </table>
 </div>
@@ -45,10 +45,10 @@ function edit_header ($olinks) {
 global $STD;
 return <<<HTML
 Edit or remove news entries.
-<div align='center'>
+<div style="margin-left:40px;">
 <br>
 <div class="rowfield">
-<table class="rowtable" cellspacing="1">
+<table class="rowtable" style="border-spacing:1px;">
 <tr>
   <td class="rowtitle" style="width:60%;"><a href="{$olinks['t']['url']}">Title</a> {$olinks['t']['img']}</td>
   <td class="rowtitle" style="width:18%;"><a href="{$olinks['u']['url']}">Author</a> {$olinks['u']['img']}</td>
@@ -86,26 +86,26 @@ global $STD;
 return <<<HTML
 Write a new news entry here.  
 <form method="post" action="{$STD->tags['root_url']}act=news&amp;param=06">
-<input type="hidden" name="security_token" value="{$token}" />
-<input type="hidden" name="nid" value="{$news["nid"]}" />
-<div align="center">
+<input type="hidden" name="security_token" value="{$token}">
+<input type="hidden" name="nid" value="{$news["nid"]}">
+<div style="margin-left:20px;">
 <br>
-<table cellspacing="0" cellpadding="1" width="90%">
+<table style="border-spacing:0px;width:90%;">
 <tr>
-  <td width="30%" class="title">Title</td>
-  <td width="70%" class="field"><input type="text" name="title" size="60" value="{$news['title']}" class="textbox" /></td>
+  <td style="width:20%;" class="title"><label for="title">Title</label></td>
+  <td style="width:80%;" class="field"><input type="text" id="title" name="title" size="60" value="{$news['title']}" class="textbox"></td>
 </tr>
 <tr>
-  <td width="30%" class="title">Content</td>
-  <td width="70%" class="field"><textarea name="content" rows="8" cols="80" class="textbox">{$news['message']}</textarea></td>
+  <td style="width:20%;" class="title"><label for="content">Content</label></td>
+  <td style="width:80%;" class="field"><textarea id="content" name="content" rows="14" cols="80" class="textbox">{$news['message']}</textarea></td>
 </tr>
 <tr>
-  <td width="30%" class="title">&nbsp;</td>
-  <td width="70%" class="field">&nbsp;</td>
+  <td style="width:20%;" class="title">&nbsp;</td>
+  <td style="width:80%;" class="field">&nbsp;</td>
 </tr>
 <tr>
-  <td width="30%" class="title">Complete Form</td>
-  <td width="70%" class="field"><input type="submit" value="Modify Entry" class="button" /></td>
+  <td style="width:20%;" class="title">Complete Form</td>
+  <td style="width:80%;" class="field"><input type="submit" value="Modify Entry" class="button"></td>
 </tr>
 </table>
 </div>
