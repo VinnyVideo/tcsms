3368b2954de42dafa27cf2c167230839b42ff26d
VinnyVideo <168298103+VinnyVideo@users.noreply.github.com>
2025-06-17 20:49:14 -0400

    Fix deprecated HTML
    Updating some deprecated HTML, and removing an unused commented-out SQL query

--- template/admin/temp_adm_main.php
+++ template/admin/temp_adm_main.php
@@ -7,10 +7,10 @@ global $STD;
 $ret = <<<HTML
 From the ACP you'll be able to manage users and submissions, update the front page, respond to messages, and control how the site operates.  You should always log out when you finish.
 <br><br>
-<div align="center">
-<table border="0" cellspacing="0" cellpadding="0" width="100%">
+<div>
+<table style="border-spacing:0px; width:100%;">
 <tr>
-  <td width="50%">
+  <td style="width:50%;padding:0px;">
     <form method="post" action="{$url}" name="notepadform">
 		<label for="notepad"><b>Staff Discussion</b></label> <input type="submit" value="Submit" class="button" tabindex="2"><br>
 		<textarea id="notepad" name="notepad" rows="2" cols="100" style="border:1px solid black; background-color:#FBFCCE;" tabindex="1"></textarea><br>
@@ -21,14 +21,14 @@ From the ACP you'll be able to manage users and submissions, update the front pa
   </td>
 </tr>
 </table><br><div class="rowfield" style="width:100%">
-<table class="rowtable" cellspacing="1" width="100%">
+<table class="rowtable" style="border-spacing:1px;width:100%;">
 HTML;
 foreach ($data as $dat) {
 		// rowcell2
 		$ret .= "<tr>
-		<td width=\"12%\" class=\"rowtitle\" align=\"right\"><a href=\"{$dat['uidurl']}\">{$dat['name']}</a></td>
+		<td style=\"width:12%;text-align:right;\" class=\"rowtitle\" ><a href=\"{$dat['uidurl']}\">{$dat['name']}</a></td>
 		<td class=\"rowcell2 canquote\" onClick=\"quote('{$dat['name']}', {$dat['id']});\">{$dat['message']}</td>
-		<td width=\"23%\" class=\"rowtitle\">{$dat['date']}<div style=\"display:none\" id=\"msg{$dat['id']}\">{$dat['raw']}</div>";
+		<td style=\"width:23%;\" class=\"rowtitle\">{$dat['date']}<div style=\"display:none\" id=\"msg{$dat['id']}\">{$dat['raw']}</div>";
 		if ($dat['uid'] == $uid) 
 			$ret .= " <a title=\"Delete\" href=\"{$delurl}&id={$dat['id']}\" style=\"color:maroon\" onClick=\"if(!confirm('Are you sure?'))return false;\">X</a>";
 		$ret .= "</td></tr>";
@@ -41,10 +41,4 @@ return $ret;
 }
 
 }
-/*CREATE TABLE `tcsms`.`tsms_chat` (
-`id` INT UNSIGNED NULL DEFAULT NULL AUTO_INCREMENT PRIMARY KEY ,
-`uid` INT UNSIGNED NOT NULL ,
-`date` INT NOT NULL ,
-`message` TEXT NOT NULL
-) ENGINE = MYISAM ;*/
 ?>
