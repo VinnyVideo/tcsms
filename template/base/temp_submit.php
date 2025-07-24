97f5f7992fb2139df063aefe69eacb8dbf786e8b
VinnyVideo <168298103+VinnyVideo@users.noreply.github.com>
2025-07-23 22:02:00 -0400

    Fix tabbing
    This seems like a good starting point for debugging some really funky code.

--- template/base/temp_submit.php
+++ template/base/temp_submit.php
@@ -140,43 +140,41 @@ function scroll_page() {
 </style>
 
 <div class='sform'>
-<form method='get' name='subselect' action='{$urlparts['form1']}' enctype='multipart/form-data'>
-{$urlparts['sess']}
-<input type='hidden' name='act' value='submit'>
-<input type='hidden' name='param' value='{$urlparts['param1']}'>
-<div class='sformstrip'>Choose a submission type.</div>
-<table class='sformtable' style='border-spacing:1px;'>
-<tr>
-  <td class='sformleft'><label for='submissiontype'>Submission Type</label></td>
-  <td class='sformright'>{$type_list}</td>
-</tr>
-</table>
-</form>
+  <form method='get' name='subselect' action='{$urlparts['form1']}' enctype='multipart/form-data'>
+    {$urlparts['sess']}
+    <input type='hidden' name='act' value='submit'>
+    <input type='hidden' name='param' value='{$urlparts['param1']}'>
+    <div class='sformstrip'>Choose a submission type.</div>
+    <table class='sformtable' style='border-spacing:1px;'>
+      <tr>
+        <td class='sformleft'><label for='submissiontype'>Submission Type</label></td>
+        <td class='sformright'>{$type_list}</td>
+      </tr>
+    </table>
+  </form>
 
-<form method='post' name='subform' action='{$urlparts['form2']}' enctype='multipart/form-data' onsubmit='return do_form()' id='uploadme'>
-<div id='mainformcont' style='position: relative;'>
-<div id='foverlay' style='position: absolute; width: 100%; height: 100%; background: rgba(0,0,0,0.6);display:none;'></div>
+  <form method='post' name='subform' action='{$urlparts['form2']}' enctype='multipart/form-data' onsubmit='return do_form()' id='uploadme'>
+    <div id='mainformcont' style='position: relative;'>
+      <div id='foverlay' style='position: absolute; width: 100%; height: 100%; background: rgba(0,0,0,0.6);display:none;'></div>
+      <input type='hidden' name='security_token' value='{$token}'>
+      <input type='hidden' name='c' value='{$STD->tags['c']}'>
+      <div id='page' style='{$def_disp['style']}'>
+        {$def_disp['module']}
+      </div>
+      <div id='select_page' style='{$def_disp['astyle']}'>
+        <div class='sformblock' style='text-align:center'><br>Select a submission type to expand the form<br>&nbsp;</div>
+      </div>
+      <div class='sformstrip' style='text-align:center'><input type='submit' value='Submit!' id='exform_submit' class='button'></div>
+    </div>
 
-<input type='hidden' name='security_token' value='{$token}'>
-<input type='hidden' name='c' value='{$STD->tags['c']}'>
-<div id='page' style='{$def_disp['style']}'>
-{$def_disp['module']}
-</div>
-<div id='select_page' style='{$def_disp['astyle']}'>
-<div class='sformblock' style='text-align:center'><br>Select a submission type to expand the form<br>&nbsp;</div>
-</div>
-<div class='sformstrip' style='text-align:center'><input type='submit' value='Submit!' id='exform_submit' class='button'></div>
-</div>
-
-<div id='exform_submsg' class='sformblock' style='display:none; text-align:center'><br>
-<div class='progress' id='progress_div' style='display:none;'>
-<div class='bar' id='bar'></div>
-<div class='percent' id='percent'>0%</div></div><br><br>
-<div id='submitstat'>Your submission is being transfered.  Please do not leave this page until the transfer is completed.</div><br>&nbsp;</div>
-<div id='foverlay' style='position: absolute; width: 100%; height: 100%; background: rgba(0,0,0,0.6);display:none;'></div>
-</form>
-
-
+    <div id='exform_submsg' class='sformblock' style='display:none; text-align:center'><br>
+    <div class='progress' id='progress_div' style='display:none;'>
+      <div class='bar' id='bar'></div>
+      <div class='percent' id='percent'>0%</div>
+    </div><br><br>
+    <div id='submitstat'>Your submission is being transfered.  Please do not leave this page until the transfer is completed.</div><br>&nbsp;</div>
+    <div id='foverlay' style='position: absolute; width: 100%; height: 100%; background: rgba(0,0,0,0.6);display:none;'></div>
+  </form>
 </div>
 ";
 }
